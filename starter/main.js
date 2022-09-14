4a059df06ffd18bbe35977afa1b9921f13fdc3a4
Clinton <clintonphillips464@gmail.com>
2022-09-14 12:57:58 +0100

    modified the code

---
+++ starter/main.js
@@ -0,0 +1,22 @@
+
+
+// basic Es5 function declaration
+// function name () {}
+var myname = "clinton401"
+function popsth() {
+    alert(myname + "  i have been coding since 12")
+}
+
+
+
+console.log(document)
+
+document.querySelector(".body")
+
+var hamburger = document.querySelector(".hamburger-container")
+
+console.log(hamburger)
+
+function addandremove() {
+hamburger.classList.toggle("showburger")
+}
