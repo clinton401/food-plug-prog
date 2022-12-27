921e2b5f2eb4eed71dba7a85d036261780232bf9
Clinton <clintonphillips464@gmail.com>
2022-12-27 16:36:57 +0100

    modified code

--- starter/main.js
+++ starter/main.js
@@ -2,22 +2,43 @@
 
 // basic Es5 function declaration
 // function name () {}
-var myname = "clinton401"
-function popsth() {
-    alert(myname + "  i have been coding since 12")
+// var myname = "clinton401"
+// function popsth() {
+//     alert(myname + "  i have been coding since 12")
+// }
+
+
+
+// console.log(document)
+
+// document.querySelector(".body")
+
+// var hamburger = document.querySelector(".hamburger-container")
+// var mobilelinkscontainer = document.querySelector(".mobile-linkscontainer")
+// console.log(hamburger)
+
+// function addandremove() {
+// hamburger.classList.toggle("showburger")
+// mobilelinkscontainer.classList.toggle("showburgerlinks")
+// }
+const menu = document.getElementById('menu');
+const mobile = document.getElementById('mobile');
+
+
+document.onclick = function(e) {
+    if(e.target.id  !== 'mobile' &&  e.target.id !== 'menu') {
+        menu.classList.remove('active');
+        mobile.classList.remove('active');
+    }
 }
 
 
 
-console.log(document)
+menu.onclick = function() {
+    menu.classList.toggle('active');
+    mobile.classList.toggle('active');
 
-document.querySelector(".body")
-
-var hamburger = document.querySelector(".hamburger-container")
-var mobilelinkscontainer = document.querySelector(".mobile-linkscontainer")
-console.log(hamburger)
-
-function addandremove() {
-hamburger.classList.toggle("showburger")
-mobilelinkscontainer.classList.toggle("showburgerlinks")
 }
+// mobile.onclick = function() {
+//     mobile.classList.toggle('active');
+// }
